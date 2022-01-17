fc01a622d106fbecee72542a680628293f018b76
Torbjorn-Rasmussen <torbjoern.r@gmail.com>
2022-01-17 14:36:08 +0100

    saving QPC objects included in datahandler

--- kwantrl/datahandling/datahandling.py
+++ kwantrl/datahandling/datahandling.py
@@ -13,80 +13,6 @@ save_data_path='/Users/qdev_26/Documents/PhD/kwantrl/kwantrl_data'
 # save_data_path="F:/qcodes_data/BBQPC2_2021/saved_data"
 
 #%%
-def load_cma_data(run_id=None,data_path=None):
-    """
-    Parameters
-    ----------
-    data_path : path to, but not including, outcmaes directory, if not provided will default to save_data_path
-    run_number : The number of the run, integer. if None provided, will return latest run
-
-    Returns
-    -------
-    numpy array
-        best fitness in iteration, sorted by iteration
-    numpy array
-        best solution in iteration, sorted by iteration
-
-    """
-    
-    if data_path==None:
-        path=save_data_path
-    else:
-        path=data_path
-        
-    if run_id==None:
-        folders=list(os.walk(path+"/outcmaes/"))[0][1]
-        folders_as_int=[int(f) for f in folders]
-        latest_run=max(folders_as_int)
-        path+='/outcmaes/{}/'.format(latest_run)
-        
-    else:
-        path+='/outcmaes/{}/'.format(run_id)
-        
-
-
-    print("data loaded from:")
-    print(path)
-    
-    with open(path+'datadict.txt','rb') as file:
-        datadict=json.load(file)
-        
-    
-    return datadict
-
-
-
-
-def unpack_cma_data(datadict,starting_point=False):
-    if starting_point:
-        start_loss=datadict['starting_point']['measurements']['0']['val']
-        start_staircase=datadict['starting_point']['measurements']['0']['staircase']
-        start_voltages=datadict['starting_point']['measurements']['0']['voltages']
-        return (start_loss,start_staircase,start_voltages)
-    
-    loss=[]
-    staircases=[]
-    voltages=[]
-    if 'x' in datadict['measurements']['0'].keys():
-        x=True
-        xs=[]
-    else:
-        x=False
-        xs=None
-
-    for key in datadict['measurements'].keys():
-
-        loss.append(datadict['measurements'][key]['val'])
-        staircases.append(datadict['measurements'][key]['staircase'])
-        voltages.append(datadict['measurements'][key]['voltages'])
-        
-        if x:
-            xs.append(datadict['measurements'][key]['x'])
-      
-    return loss,staircases,voltages,xs
-
-    
-    
 class datahandler():
     def __init__(self,data_path=None):
         
@@ -144,6 +70,24 @@ class datahandler():
     def load_transformed_data(self,run_id=None,folder=None):
         return self.transform_data(self.load_data(run_id,folder))
 
+    def save_qpc(self,QPC,run_id=None,folder=None):
+        if run_id!=None:
+            folder=f"{self.data_path}outcmaes/{run_id}/"
+        elif run_id==None & folder == None:
+            folder=self.latest_outcmaes_folder_name()
+        
+        with open(folder+'qpc.pkl','wb') as file:
+            pickle.dump(QPC,file)
+
+    def load_qpc(self,run_id=None,folder=None):
+        if run_id!=None:
+            folder=f"{self.data_path}outcmaes/{run_id}/"
+        elif run_id==None & folder == None:
+            folder=self.latest_outcmaes_folder_name()
+        
+        with open(folder+'qpc.pkl','rb') as file:
+            QPC=pickle.load(file)
+        return QPC
 
 
     
